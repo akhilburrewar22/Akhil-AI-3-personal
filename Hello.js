93bd1b25638efad0ed5e551e5e4b902f94c76bc4
akhilburrewar22 <akhilburrewar22@gmail.com>
2026-04-13 08:56:32 +0000

    changed

--- Hello.js
+++ Hello.js
@@ -1,3 +1,11 @@
 function clickme(){
     alert("Button clicked");
 }
+
+function showH1(){
+    document.getElementById("myH1").style.display = "block";
+}
+
+function hideH1(){
+    document.getElementById("myH1").style.display = "none";
+}
